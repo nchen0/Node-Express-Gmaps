d3d5841b20db352d926e073c4205738d174dbacc
nchen0 <nicky.chen0@gmail.com>
2018-07-29 15:40:31 -0700

    search

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const fetch = require("node-fetch");
 
 const server = express();
+server.use(express.json());
 
 server.get("/places", (req, res) => {
-  res.send("test working");
-  res.end();
+  let name;
+  fetch(
+    "https://maps.googleapis.com/maps/api/place/textsearch/json?query=copenhagent&key=AIzaSyA1W5ZgRDi_tSLOQoDkpA-MC_x7dmHgTjM"
+  )
+    .then(response => res.status(200).json({ name: response.data.results[0].name }))
+
+    .catch(err => {
+      res.status(404).json({ message: err.message });
+    });
 });
 
 server.listen(8000, () => console.log("API running on port 8000"));
